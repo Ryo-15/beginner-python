712d92837f594ada23b024a1268a413c84b525a1
Ryo-15 <tomorrows.way15@gmail.com>
2020-10-26 11:13:45 +0900

    [finish]beginner-python_lesson_2

---
+++ Main.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+#HTMLを表示する
+print("<h1>hello world</h1>")
+print("<p>世界のみなさん")
+print("<b>こんにちは</b></p>")
+
+# coding: utf-8
+# 変数を使う
+player = "戦士"
+print(player + "は、荒野を歩いていた")
+print(player + "は、モンスターと戦った")
+print(player + "は、モンスターをたおした")
+
