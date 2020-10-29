b924ae057045a5caac52bb9d085d8ac462bbe4ff
Ryo-15 <tomorrows.way15@gmail.com>
2020-10-29 15:19:52 +0900

    update

--- Main.py
+++ Main.py
@@ -11,3 +11,16 @@ print(player + "は、荒野を歩いていた")
 print(player + "は、モンスターと戦った")
 print(player + "は、モンスターをたおした")
 
+# coding: utf-8
+#数の表示とサイコロ
+import random
+number = random.randint(1, 100)
+print("スライムが" + str(number) + "匹現れた")
+
+# coding: utf-8
+# 計算する
+number = 100 + 40
+print(number + 30 * 3)
+print(number)
+
+
