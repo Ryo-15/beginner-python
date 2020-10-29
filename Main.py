98d6524be432c0bf1df8090716cf9f25bb874cab
Ryo-15 <tomorrows.way15@gmail.com>
2020-10-29 15:50:42 +0900

    update

--- Main.py
+++ Main.py
@@ -23,4 +23,14 @@ number = 100 + 40
 print(number + 30 * 3)
 print(number)
 
-
+# coding: utf-8
+#値段を計算する
+#apple_price	リンゴの単価
+#apple_num	リンゴを買う数
+import random
+apple_price = 350
+apple_num = random.randint(1, 10)
+print("リンゴの単価：" + str(apple_price) + "円")
+print("リンゴを買う数：" + str(apple_num) + "個")
+total = apple_price * apple_num
+print("合計金額：" + str(total) + "円")
