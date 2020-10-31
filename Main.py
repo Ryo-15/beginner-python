cec783f5bb1cb34d28a7de3e2285c5d9ee6632e1
Ryo-15 <tomorrows.way15@gmail.com>
2020-10-31 13:51:09 +0900

    update

--- Main.py
+++ Main.py
@@ -70,3 +70,22 @@ elif time == 12:
     print("正午")
 elif time > 12:
     print("午後")
+
+# coding: utf-8
+# おみくじを作る
+# 比較演算子 == > < >= <= !=
+# 大吉  中吉  小吉  凶  大凶
+import random
+omikuji = random.randint(1, 10)
+print(omikuji)
+
+if omikuji == 1:
+    print("大吉")
+elif omikuji == 2:
+    print("中吉")
+elif omikuji <= 4:
+    print("小吉")
+elif omikuji <= 7:
+    print("凶")
+else:
+    print("大凶")
