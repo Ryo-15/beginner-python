995d3cf5b0e946d95461c1c7d1b605a25bd2b5d6
Ryo-15 <tomorrows.way15@gmail.com>
2020-11-03 16:34:42 +0900

    update

--- Main.py
+++ Main.py
@@ -185,3 +185,12 @@ b = int(input())
 for i in range(a, b + 1):
   print(i)
 
+# coding: utf-8
+# 西暦年と平成年の対応表を作る
+# 1989年から2016年までをループで出力
+# ループ内で、各西暦年を平成年に変換
+for seireki in range(1989, 2020):
+    print("西暦" + str(seireki) + "年は、", end = "")
+    heisei = seireki - 1989
+    print("平成" + str(heisei) + "年です。")
+
