d94e9643824a9a0af8472fd20e7a9748c428351b
Ryo-15 <tomorrows.way15@gmail.com>
2020-11-03 14:05:24 +0900

    update

--- Main.py
+++ Main.py
@@ -166,3 +166,14 @@ print("</select>")
 line = input()
 print("hello" + line)
 
+# coding: utf-8
+# 標準入力とループ処理
+# line = input()
+# print("hello " + line)
+
+count = int(input())
+# print("データ個数 " + str(count))
+for i in range(count):
+    line = input().rstrip()
+    print(line + "は、スライムを攻撃した！")
+
