10d7e82477de85d36a6ed42a41b4127dc7931392
Ryo-15 <tomorrows.way15@gmail.com>
2020-11-04 16:34:53 +0900

    update

--- Main.py
+++ Main.py
@@ -219,3 +219,13 @@ print(player_2)
 team = ["勇者", "魔法使い"]
 print(team)
 
+# coding: utf-8
+# リストの要素を取り出す
+
+team = ["勇者", "魔法使い"]
+print(team)
+print(team[0])
+num = 0
+print(team[num + 1])
+print(len(team))
+
