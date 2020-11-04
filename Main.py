e022e9faa264ea4ededbf3ea804172457fc0d06c
Ryo-15 <tomorrows.way15@gmail.com>
2020-11-04 17:00:08 +0900

    update

--- Main.py
+++ Main.py
@@ -229,3 +229,24 @@ num = 0
 print(team[num + 1])
 print(len(team))
 
+# coding: utf-8
+# リストの要素を操作する
+
+team = ["勇者", "魔法使い"]
+print(team)
+print(team[0])
+print(len(team))
+
+team.append("戦士")
+print(team)
+print(len(team))
+
+team[2] = "ドラゴン"
+print(team)
+print(len(team))
+
+team.pop(2)
+print(team)
+print(len(team))
+
+
