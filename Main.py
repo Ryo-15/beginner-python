edf2b3061b2dc07248c25b533145a4bc3f7aa7c6
Ryo-15 <tomorrows.way15@gmail.com>
2020-11-07 10:26:09 +0900

    update

--- Main.py
+++ Main.py
@@ -302,3 +302,12 @@ for line in sys.stdin.readlines():
 
 print(array)
 
+# coding: utf-8
+# 複数行のカンマ区切りデータを出力する
+
+import sys
+for line in sys.stdin.readlines():
+	# ここに、文字列を分割して、出力するコードを書く
+	enemy = line.rstrip().split(",")
+	print(enemy[0] + "が" + enemy[1] + "匹現れた")
+
