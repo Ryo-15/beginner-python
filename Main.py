79983f5a852c441f899a491f0c34a39cce21e323
Ryo-15 <tomorrows.way15@gmail.com>
2020-11-08 16:58:20 +0900

    update

--- Main.py
+++ Main.py
@@ -329,3 +329,22 @@ attack = random.randrange(num)
 
 # 選んだ的に、「会心の一撃！」と表示
 print(line[attack] + "に会心の一撃！" + line[attack] + "を倒した！")
+
+# coding: utf-8
+# じゃんけんプログラム
+
+import random
+# 標準入力から1行取得
+line = input().rstrip()
+
+# カンマで分割して、リストに代入
+janken = line.split(",")
+
+# リストの要素数を変数に代入
+num = len(janken)
+
+# リストの中身を出力
+print(janken)
+
+# ランダムに選んだリストの要素を出力
+print(janken[random.randrange(num)])
