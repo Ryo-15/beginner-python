7c94a5255a9a4c1726178f9835b4fa7438b420df
Ryo-15 <tomorrows.way15@gmail.com>
2020-11-09 21:03:10 +0900

    update

--- Main.py
+++ Main.py
@@ -368,3 +368,19 @@ print(omikuji)
 
 # ランダムに選んだリストの要素を出力
 print(omikuji[random.randrange(num)])
+
+# coding: utf-8
+# Your code here!
+
+# リストのおさらい
+enemyArray = ["スライム", "モンスター", "ドラゴン"]
+print(enemyArray)
+print(enemyArray[0])
+
+# 辞書の具体例
+enemeyDictionary = {"ザコ":"スライム", "中ボス":"ドラゴン", "ラスボス":"魔王"}
+print(enemeyDictionary)
+print(enemeyDictionary["中ボス"])
+
+level = "ザコ"
+print(enemeyDictionary[level])
