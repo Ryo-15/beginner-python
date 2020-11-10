4437f06effab4a57576b4145044dc20b3144f54a
Ryo-15 <tomorrows.way15@gmail.com>
2020-11-10 14:07:06 +0900

    update

--- Main.py
+++ Main.py
@@ -426,3 +426,10 @@ for rank in enemies:
     print(enemies[rank] + "が、あらわれた！")
 for (rank, enemy) in enemies.items():
     print(rank + "の" + enemy + "が、あらわれた！")
+
+# ループで辞書のキーと値を出力しよう
+
+skills = {"職業" : "戦士", "体力" : 100, "魔法力" : 200, "ゴールド" : 380}
+# この下で、ハッシュの値をループで出力してみよう
+for (a, skill) in skills.items():
+    print(a + "は" + str(skill) + "です")
