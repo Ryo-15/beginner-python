9fd9fcc6cd52ee01a35a5d8e417446557fc59262
Ryo-15 <tomorrows.way15@gmail.com>
2020-11-12 14:03:21 +0900

    update

--- Main.py
+++ Main.py
@@ -486,3 +486,28 @@ math = {"えんどう" : 99, "あだち" : 40, "いいだ" : 12}
 
 # この下で、辞書をキーでソートして、辞書として出力しよう
 print(sorted(math.items()))
+
+
+# coding: utf-8
+# Your code here!
+
+# 画像用ハッシュ
+item_images = {
+    "剣":"http://paiza.jp/learning/images/sword.png",
+    "盾":"http://paiza.jp/learning/images/shield.png",
+    "回復薬":"http://paiza.jp/learning/images/potion.png",
+    "クリスタル":"http://paiza.jp/learning/images/crystal.png"
+}
+
+# アイテムの並び順配列
+item_orders = ["クリスタル", "盾", "剣", "回復薬", "回復薬", "回復薬"]
+
+print(item_images)
+print(item_orders)
+
+# アイテム名を取り出す
+for item_name in item_orders:
+    # print(item_name)
+# 画像ファイル名を取り出す
+    print("<img src='" + item_images[item_name] + "'>")
+    print(item_name + "<br>")
