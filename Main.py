72b2f5fc623221f0b75a418851b7dd0dd88a53f7
Ryo-15 <tomorrows.way15@gmail.com>
2020-11-12 14:48:54 +0900

    update

--- Main.py
+++ Main.py
@@ -511,3 +511,18 @@ for item_name in item_orders:
 # 画像ファイル名を取り出す
     print("<img src='" + item_images[item_name] + "'>")
     print(item_name + "<br>")
+
+# 画像用辞書
+items_imges = {
+    "剣" : "http://paiza.jp/learning/images/sword.png",
+    "盾" : "http://paiza.jp/learning/images/shield.png",
+    "回復薬" : "http://paiza.jp/learning/images/potion.png",
+    "クリスタル" : "http://paiza.jp/learning/images/crystal.png"
+}
+
+# アイテムの並び順リスト
+items_orders = ["剣", "盾", "回復薬", "クリスタル"]
+
+# ここから下を記述しよう
+for items_img in items_imges:
+    print("<img src='" + items_imges[items_img] + "'><br>")
