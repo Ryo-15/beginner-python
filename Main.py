476b8ab39c0c63fcbe62887b0d0b211b2f2cfe3e
Ryo-15 <tomorrows.way15@gmail.com>
2020-11-15 17:16:08 +0900

    update

--- Main.py
+++ Main.py
@@ -545,3 +545,27 @@ while item_cnt > 0:
   item = input()
   print("<img src = '" + items_imges[item] + "'>")
   item_cnt = item_cnt - 1
+
+
+# coding: utf-8
+# Your code here!
+
+# 2次元リストを作成する
+player = "盗賊"
+team_a = [player, "勇者", "戦士", "魔法使い"]
+print(team_a)
+print(team_a[0])
+
+team_b = [team_a[0], team_a[1], team_a[2]]
+print(team_b)
+print(team_b[1])
+
+team_c = ["勇者", "戦士", "魔法使い"]
+team_d = ["盗賊", "忍者", "商人"]
+team_e = ["スライム", "ドラゴン", "魔王"]
+
+teams = [team_c, team_d, team_e]
+print(teams)
+print(teams[0])
+print(teams[0][0])
+print(teams[0][2])
