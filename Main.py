d5108ee7f49834a28047ed907e0da1bbf73c903f
Ryo-15 <tomorrows.way15@gmail.com>
2020-11-16 20:02:18 +0900

    update

--- Main.py
+++ Main.py
@@ -589,3 +589,26 @@ teams[0][0] = "魔導士"
 print(teams)
 print(len(teams))
 print(len(teams[0]))
+
+
+# coding: utf-8
+# Your code here!
+
+# 2次元リストの基本操作
+
+teams = [["勇者", "戦士"], ["盗賊", "忍者", "商人"], ["スライム", "ドラゴン", "魔王"], ["魔法使い"]]
+print(teams)
+
+teams.append(["メタルモンスター", "シルバーモンスター", "ブラックモンスター"])
+print(teams)
+print(len(teams))
+print(len(teams[0]))
+
+del teams[1]
+print(teams)
+print(len(teams))
+
+del teams[0][1]
+print(teams)
+print(len(teams))
+print(len(teams[0]))
