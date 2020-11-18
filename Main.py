83059bdeba6201da5445f0dacc00ab6e45abb2cf
Ryo-15 <tomorrows.way15@gmail.com>
2020-11-18 13:57:39 +0900

    update

--- Main.py
+++ Main.py
@@ -651,3 +651,12 @@ for item in numbers:
     results.append(item * 10)
     
 print(results)
+
+
+#リストの中身をループで表示する
+
+enemies = ["スライム", "モンスター", "ゾンビ", "ドラゴン", "魔王"]
+# ここに、要素をループで表示するコードを記述する
+for (i, enemy) in enumerate(enemies):
+    print(str(i + 1) + "番目の" + enemy + "が現れた")
+
