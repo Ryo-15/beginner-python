b4b60d5dd8ad62a54919ca186d58131b08029819
Ryo-15 <tomorrows.way15@gmail.com>
2020-11-19 22:02:50 +0900

    update

--- Main.py
+++ Main.py
@@ -673,3 +673,17 @@ for item in numbers:
 
 print(numbers2)
 
+
+# coding: utf-8
+# Your code here!
+
+# 2次元リストをforで作成する
+# numbers = [1, 2, 3]
+numbers = [i * 2 for i in range(10)]
+print(numbers)
+print(len(numbers))
+
+numbers2 = [[1 for i in range(3)] for j in range(4)]
+numbers2[0][1] = 2
+print(numbers2)
+
