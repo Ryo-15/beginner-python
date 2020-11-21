4692bf0fd185b500b4916ee14f6be7619d21b897
Ryo-15 <tomorrows.way15@gmail.com>
2020-11-21 19:46:38 +0900

    update

--- Main.py
+++ Main.py
@@ -719,3 +719,23 @@ for line in enemy_img:
             print(" ", end="")
     print()
 
+
+# ドットで文字を出力しよう
+
+letter_A = [[0,0,1,1,0,0],
+            [0,1,0,0,1,0],
+            [1,0,0,0,0,1],
+            [1,1,1,1,1,1],
+            [1,0,0,0,0,1],
+            [1,0,0,0,0,1]]
+
+# ここに、ドットを表示するコードを記述する
+for line in letter_A:
+    for dot in line:
+        if dot == 1:
+            print("@", end="")
+        else:
+            print(" ",end="")
+    print("")
+
+
