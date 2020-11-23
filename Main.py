85acfd503ea396da25caf2c9e2bbff5b5919816e
Ryo-15 <tomorrows.way15@gmail.com>
2020-11-23 17:18:55 +0900

    update

--- Main.py
+++ Main.py
@@ -846,3 +846,15 @@ for i,line in enumerate(landmap):
     for area in line:
         print(area, end="")
     print()
+
+
+# 複数行テキストに行番号を追加する
+
+text = ["吾輩は猫である",
+        "名前はまだ無い",
+        "どこで生まれたか",
+        "とんと見当がつかぬ"]
+
+#ここに、行番号を表示するコードを記述する
+for i,line in enumerate(text):
+    print(str(i + 1) + ":" + line)
