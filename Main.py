71f5124006815dd675afbf1512a91cfad31ade65
Ryo-15 <tomorrows.way15@gmail.com>
2020-11-24 21:27:06 +0900

    update

--- Main.py
+++ Main.py
@@ -858,3 +858,23 @@ text = ["吾輩は猫である",
 #ここに、行番号を表示するコードを記述する
 for i,line in enumerate(text):
     print(str(i + 1) + ":" + line)
+
+
+# coding: utf-8
+# Your code here!
+
+# 2次元リストで地図を表示する
+
+landmap = [["森" for i in range(20)] for j in range(10)]
+landmap[0][0] = "城"
+landmap[0][19] = "町"
+landmap[9][19] = "町"
+for i,line in enumerate(landmap):
+    print(str(i) + ":", end="")
+    for j,area in enumerate(line):
+        if (i % 2 == 0 or j % 3 == 0) and area == "森":
+            print("＋", end="")
+        else:
+            print(area, end="")
+    print()
+
